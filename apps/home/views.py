7de9c7c8e1302d6984a1e70e25da1ff87ef8e978
hagid-786 <92071690+hagid-786@users.noreply.github.com>
2022-04-05 00:18:37 +0800

    add sidebar

--- apps/home/views.py
+++ apps/home/views.py
@@ -11,7 +11,7 @@ from django.urls import reverse
 from .models import Profile
 
 
-@login_required(login_url="/login/")
+
 def index(request):
     context = {'segment': 'index',
                'user_list': Profile.objects.all()}
@@ -20,7 +20,7 @@ def index(request):
     return HttpResponse(html_template.render(context, request))
 
 
-@login_required(login_url="/login/")
+
 def pages(request):
     context = {}
     # All resource paths end in .html.

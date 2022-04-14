e2a2e557e65a1a1b7a69ce8e4208f57b70b7c196
Nizar <nizarhaider@gmail.com>
2022-04-15 02:25:28 +0800

    updated filter frontend

--- apps/home/models.py
+++ apps/home/models.py
@@ -2,7 +2,6 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-
 from django.db import models
 from django.contrib.auth.models import User
 
--- apps/home/views.py
+++ apps/home/views.py
@@ -2,7 +2,8 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-
+from django.db.models import Q, Count
+from django.shortcuts import render, get_object_or_404
 from django import template
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
@@ -11,10 +12,29 @@ from django.urls import reverse
 from .models import Profile
 
 
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
+
+def filter(request):
+    qs = Profile.objects.all()
+    categories = Category.objects.all()
+    title_contains_query = request.GET.get('title_contains')
+    id_exact_query = request.GET.get('id_exact')
+    title_or_author_query = request.GET.get('title_or_author')
+    view_count_min = request.GET.get('view_count_min')
+    view_count_max = request.GET.get('view_count_max')
+    date_min = request.GET.get('date_min')
+    date_max = request.GET.get('date_max')
+    category = request.GET.get('category')
+    reviewed = request.GET.get('reviewed')
+    not_reviewed = request.GET.get('notReviewed')
+
 
 def index(request):
     context = {'segment': 'index',
                'user_list': Profile.objects.all()}
+
 
     html_template = loader.get_template('home/homepage.html')
     return HttpResponse(html_template.render(context, request))
